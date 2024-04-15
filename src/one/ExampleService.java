f3a2a5e5e8a9094530853dd59fa2d280bd75433c
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2024-04-15 21:11:09 +0300

    HW v 1.00

---
+++ src/one/ExampleService.java
@@ -0,0 +1,11 @@
+package one;
+
+public class ExampleService {
+    static boolean checkField(String s) {
+        return s.length() > 2;
+    }
+
+    static boolean checkField(int number) {
+        return number > 0;
+    }
+}
