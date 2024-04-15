248fc6b99548ca791e568db3d9accb860e68427a
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2024-04-15 21:40:16 +0300

    sout name of Example

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(Example.class.getClassLoader());
         System.out.println(Example.class.getClassLoader().getName());
+        System.out.println(Example.class.getName());
     }
 }
