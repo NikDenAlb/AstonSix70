2612ed3883f4a60b192db25197372210b16b90dd
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2024-04-28 21:44:33 +0300

    simple Shellsort

---
+++ src/four/ServiceSort.java
@@ -0,0 +1,19 @@
+package four;
+
+import org.jetbrains.annotations.NotNull;
+
+public class ServiceSort {
+    public static <T extends Comparable<T>> void sort(T @NotNull [] arr) {
+        for (int gap = arr.length >> 1; gap > 0; gap >>= 1) {
+            for (int i = gap; i < arr.length; i++) {
+                int j = i;
+                while (j >= gap && arr[j].compareTo(arr[j - gap]) < 0) {
+                    T temp = arr[j];
+                    arr[j] = arr[j - gap];
+                    arr[j - gap] = temp;
+                    j -= gap;
+                }
+            }
+        }
+    }
+}
