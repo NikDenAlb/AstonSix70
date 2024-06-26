1bf412e7b4ed42a84d3576a54dc1d5c0e78ff727
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2024-06-26 17:12:14 +0300

    add(int index, E e)

--- src/stage2/mylists/MyLinkedList.java
+++ src/stage2/mylists/MyLinkedList.java
@@ -90,14 +90,48 @@ public class MyLinkedList<E extends Comparable<E>> {
         return true;
     }
 
+    /**
+     * Inserts the specified element at the specified position in this
+     * list. Shifts the element currently at that position (if any) and
+     * any subsequent elements to the right (adds one to their indices).
+     *
+     * @param index index at which the specified element is to be inserted
+     * @param e     element to be inserted
+     * @throws IndexOutOfBoundsException if the {@code index} is out of range
+     */
+    public void add(int index, E e) {
+        Objects.checkIndex(index, size + 1);
+        if (size == 0 || index == size) {
+            this.add(e);
+            return;
+        }
+        if (index == 0) {
+            Node<E> newNode = new Node<>(null, e, getNode(index));
+            getNode(index).next = newNode;
+            first = newNode;
+            size++;
+            return;
+        }
+        Node<E> newNode = new Node<>(getNode(index - 1), e, getNode(index));
+        getNode(index - 1).next = newNode;
+        getNode(index).prev = newNode;
+        size++;
+    }
+
+
     @Override
     public String toString() {
+        if (size == 0) {
+            return "[]";
+        }
         StringBuilder s = new StringBuilder();
-        for (int i = 0; i < size-1; i++) {
+        s.append("[");
+        for (int i = 0; i < size - 1; i++) {
             s.append(getNode(i));
             s.append(" ");
         }
         s.append(getNode(size - 1));
+        s.append("]");
         return s.toString();
     }
 }
