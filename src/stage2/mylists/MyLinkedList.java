d248f2bffee3dc8b35c6476b9a566df9fabb0fb3
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2024-06-26 18:08:20 +0300

    0.20 sort()

--- src/stage2/mylists/MyLinkedList.java
+++ src/stage2/mylists/MyLinkedList.java
@@ -172,6 +172,46 @@ public class MyLinkedList<E extends Comparable<E>> {
         last = null;
     }
 
+    /**
+     * Sorts the array using the QuickSort algorithm.
+     */
+    public void sort() {
+        quicksort(0, size - 1);
+    }
+
+    private void quicksort(int low, int high) {
+        if (low >= high) {
+            return;
+        }
+        int pivot = partition(low, high);
+        quicksort(low, pivot - 1);
+        quicksort(pivot + 1, high);
+    }
+
+    private int partition(int low, int high) {
+        int pivotIndex = (low + high) / 2;
+        swap(pivotIndex, high);
+        int pivotIndexCounter = low;
+        for (int i = low; i < high; i++) {
+            if (this.get(i).compareTo(this.get(high)) <= 0) swap(pivotIndexCounter++, i);
+        }
+        swap(pivotIndexCounter, high);
+        return pivotIndexCounter;
+    }
+
+    private void swap(int firstIndex, int secondIndex) {
+        if (firstIndex != secondIndex) {
+            E temp = this.get(firstIndex);
+            this.set(firstIndex, this.get(secondIndex));
+            this.set(secondIndex, temp);
+        }
+    }
+
+    public void set(int index, E e) {
+        Objects.checkIndex(index, size);
+        getNode(index).e = e;
+    }
+
     @Override
     public String toString() {
         if (size == 0) {
